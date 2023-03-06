3f21deafc7c38d20fc69f7c512dad5ae690a9206
Joel Manga <jmanga86@gmail.com>
2023-03-06 16:28:28 +0300

    todo file

---
+++ Todo.java
@@ -0,0 +1,10 @@
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Boolean completed;
+    // getters and setters
+}
